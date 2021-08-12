589a025c6341aa65d0009843b907d456ecad0236
AkshatBaranwal <31817177+AkshatBaranwal@users.noreply.github.com>
2021-08-12 12:04:35 +0530

    fix automatic highlighting

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
-var date = new Date()
-var hrs = date.getHours()
-var min = date.getMinutes()
-var day = date.getDay()
-let k = -1
-if (hrs == 9 || hrs == 10)
-  k = 0
-else if ((hrs == 11 && min >= 10) || hrs == 12 || (hrs == 13 && min <= 10))
-  k = 1;
-else if (hrs == 15 || hrs == 16)
-  k = 2;
-if(k != -1)
-  document.querySelectorAll(".guide-slot_card > h4:first-child")[day*3-3+k].style.color = "darkcyan"
+var date = new Date();
+var hrs = date.getHours();
+var min = date.getMinutes();
+var day = date.getDay();
+let k = -1;
+
+if (hrs >= 8 && hrs <= 12) {
+  k = hrs - 8;
+} else if (hrs >= 15 && hrs <= 18) {
+  k = Math.floor((hrs + 1) / 2) - 2;
+}
+
+if (k != -1 && day < 6)
+  document.querySelectorAll(".guide-slot_card > h4:first-child")[k + day * 8 - 8].style.color = "darkcyan";
